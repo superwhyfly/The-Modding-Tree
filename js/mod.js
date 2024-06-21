4a72822848596819500753143043c1bfad0eab2e
superwhyfly <halomudahmudahancepatlol@gmail.com>
2024-06-21 14:30:20 +0700

    Nothing/Released
    epic

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name:"The, Project Tree",
+	id :"TheCOOLid"
+	,author: "nobody",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
